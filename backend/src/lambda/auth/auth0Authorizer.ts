26062334a9540c6e79a1447352060487a9602804
DeltaDivine99 <deltadivine99@gmail.com>
2022-09-28 00:07:37 +0700

    fix

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -17,6 +17,7 @@ const jwksUrl = 'https://dev-7h2rt62q.us.auth0.com/.well-known/jwks.json'
 export const handler = async (
   event: CustomAuthorizerEvent
 ): Promise<CustomAuthorizerResult> => {
+  logger.info('event', event)
   logger.info('Authorizing a user', event.authorizationToken)
   try {
     const jwtToken = await verifyToken(event.authorizationToken)
@@ -56,14 +57,15 @@ export const handler = async (
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
+  logger.info('token', token)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
-
+  logger.info('jwt', jwt)
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
   const kid = jwt.header.kid
   const cert = await getSigningKey(kid)
-  return verify(token, cert, {algorithms: ['RS256']}) as JwtPayload
+  return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
@@ -79,13 +81,37 @@ function getToken(authHeader: string): string {
 }
 
 async function getSigningKey(kid: String): Promise<string> {
-  const jwksRes = await Axios.get(jwksUrl)
-  logger.info('jwksRes', jwksRes)
+  logger.info('kid', kid)
+  const jwksRes = await Axios.get(jwksUrl, {
+    headers: {
+      'Content-Type': 'application/json',
+      "Access-Control-Allow-Origin": "*",
+      'Access-Control-Allow-Credentials': true,
+    }
+  })
+  logger.info(jwksRes)
   if (jwksRes.status != 200) throw new Error(jwksRes.statusText)
-  const key = jwksRes.data.keys(k => {
-    k.kid = kid
-  }).x5c
-  logger.info('keys', key)
-  if (!key) throw new Error('get key fail')
-  return key
+  logger.info('jwksRes keys', jwksRes.data.keys)
+  // const key = jwksRes.data.keys(k => {
+  //   k.kid = kid
+  // }).x5c
+  const filteredKeys = jwksRes.data.keys.filter(key =>
+    key.alg === 'RS256'
+    && key.kty === 'RSA'
+    && key.use === 'sig'
+    && key.kid === kid
+    && ((key.x5c && key.x5c.length))
+  ).map(key => {
+    return { 
+      kid: key.kid,
+      nbf: key.nbf,
+      x5c: key.x5c[0]
+    };
+  });
+  logger.info('filteredKeys', filteredKeys)
+  const x5cKey = filteredKeys[0].x5c
+  logger.info('key', x5cKey)
+  if (!x5cKey) throw new Error('get key fail')
+  const cert = `-----BEGIN CERTIFICATE-----\n${x5cKey}\n-----END CERTIFICATE-----\n`;
+  return cert
 }
